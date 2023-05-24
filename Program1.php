31fb64697dd502d1ad5c9bb67a01aa7ad54d08d7
Paul-Wesley Jeanty <jpwesley530@gmail.com>
2023-05-23 23:18:02 -0400

    Exo 1 unit 7

---
+++ Program1.php
@@ -0,0 +1,13 @@
+<?php
+// Initialize the sum variable to zero
+$sum = 0;
+
+// Loop from 0 to 50
+for ($i = 0; $i <= 50; $i++) {
+    // Add the current value of i to the sum
+    $sum = $sum + $i;
+}
+
+// Display the sum
+echo "The sum of all the integers between 0 and 50 is: $sum";
+?>
